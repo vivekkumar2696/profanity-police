1bb4057f3d6a3fba609d5845a8db7fec8d8efab2
Vivek <kumar.vivek2696@gmail.com>
2021-06-20 12:36:06 +0530

    [youtube.py] Add URL support along with video id

--- profanity_police/youtube.py
+++ profanity_police/youtube.py
@@ -1,8 +1,23 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
+from urllib import parse
+
 class YoutubeTranscript:
-    def __init__(self, video_id):
-        self.video_id = video_id
+    def __init__(self, url = None, video_id = None):
+        """ 
+            Constructor
+
+            :param video_id: Video ID of URL
+        """
+        if not video_id and not url:
+            # raise exception
+            pass
+
+        if video_id:
+            self.video_id = video_id
+
+        if url:
+            self.video_id = self.extract_video_id(url)
 
     def get_original_transcript(self, language_code = 'en'):
         """
@@ -18,7 +33,30 @@ class YoutubeTranscript:
         for transcript in transcript_list:
             if transcript.language_code == language_code:
                 return transcript.fetch()
-        # transcript = transcript_list.find_generated_transcript(['en'])
+        return None
+
+    def extract_video_id(self, value):
+        """
+        Credits: https://stackoverflow.com/questions/4356538/how-can-i-extract-video-id-from-youtubes-link-in-python
+
+        Examples:
+        - http://youtu.be/SA2iWivDJiE
+        - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+        - http://www.youtube.com/embed/SA2iWivDJiE
+        - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+        """
+        query = parse.urlparse(value)
+        if query.hostname == 'youtu.be':
+            return query.path[1:]
+        if query.hostname in ('www.youtube.com', 'youtube.com'):
+            if query.path == '/watch':
+                p = parse.parse_qs(query.query)
+                return p['v'][0]
+            if query.path[:7] == '/embed/':
+                return query.path.split('/')[2]
+            if query.path[:3] == '/v/':
+                return query.path.split('/')[2]
+        # fail?
         return None
 
     def get_transcript(self, language_code = 'en'):
--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from profanity_police.checker import Checker
 from profanity_police.youtube import YoutubeTranscript
 
-y_transcript = YoutubeTranscript("Vev2ybF2Z6g")
+y_transcript = YoutubeTranscript(url = "https://www.youtube.com/watch?v=Vev2ybF2Z6g&ab_channel=AllIndiaBakchod", video_id = "Vev2ybF2Z6g")
 
 checker = Checker()
 transcript = y_transcript.get_transcript(language_code = "en-GB")
