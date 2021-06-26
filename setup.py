225a9cc25573d3da12d29db13d4765da8d17fc4c
Vivek <kumar.vivek2696@gmail.com>
2021-06-27 00:34:19 +0530

    [setup.py] Remove alpha

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
     install_requires = required,
     keywords = ["transcript", "profanity", "profanity-detection", "swearword", "srt-parser", "youtube-transcripts", "youtube-captions"],
     classifiers=[
-        'Development Status :: 3 - Alpha',# Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
