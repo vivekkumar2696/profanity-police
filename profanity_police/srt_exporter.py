23ff1a2e78bb0d99447639a8efed08ccecfcba2b
Vivek <kumar.vivek2696@gmail.com>
2021-06-27 01:43:45 +0530

    [srt_export.py] Round milliseconds

--- profanity_police/srt_exporter.py
+++ profanity_police/srt_exporter.py
@@ -6,7 +6,6 @@ class SrtExporter():
         hours = seconds//3600
         mins = (seconds % 3600)//60
         secs = int((seconds % 3600)%60)
-        print(hours, mins, secs)
         return (hours, mins, secs)
 
     def export(self, transcript_json, encoding = None):
@@ -19,19 +18,17 @@ class SrtExporter():
         i = 1
         file = ''
         for data in transcript_json:
-            start = data['start'] # Get start time
-            stop = data['end'] # Get the end time
-            content = data['text'] # Get subtitle content
-            file +='{}\n'.format(i) # Add serial number
+            start = data['start']
+            stop = data['end']
+            content = data['text']
+            file +='{}\n'.format(i)
             converted_start_time = self.sec_to_hms(start)
             hour = converted_start_time[0]
             minute = converted_start_time[1]
             sec = converted_start_time[2]
 
-            print("@", start)
-            minisec = int(math.modf(start)[0] * 1000) # Processing start time
-            print(minisec)
-            file += str(hour).zfill(2) +':' + str(minute).zfill(2) +':' + str(sec).zfill(2) +',' + str(minisec). zfill(2) # Fill the number with 0 and write in the format
+            minisec = int(math.modf(start)[0] * 1000)
+            file += str(hour).zfill(2) +':' + str(minute).zfill(2) +':' + str(sec).zfill(2) +',' + str(minisec). zfill(3)
 
             file += ' --> '
             
@@ -40,9 +37,9 @@ class SrtExporter():
             minute = converted_stop_time[1]
             sec = converted_stop_time[2]
 
-            minisec = abs(int(math.modf(stop)[0] * 1000)) # Here minus 1 is to prevent two subtitles from appearing at the same time
-            file += str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':' + str(sec).zfill(2) + ',' + str(minisec).zfill(2)
-            file +='\n' + content +'\n\n' # add subtitle text
+            minisec = abs(int(math.modf(stop)[0] * 1000)) + 1
+            file += str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':' + str(sec).zfill(2) + ',' + str(minisec).zfill(3)
+            file +='\n' + content +'\n\n'
             i += 1
 
         with open('test.srt', 'w', encoding='utf-8') as f:
